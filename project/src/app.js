8cd047df235c2f8102d12696317cd3ee01b12d85
DmitriiThe <>
2024-06-06 01:45:55 +0300

    making users.json and getUsers function

--- project/src/app.js
+++ project/src/app.js
@@ -1,11 +1,16 @@
 const http = require("http");
+const fs = require("fs");
+
+const getUsers = () => {
+  return fs.readFileSync("../data/users.json");
+}
 
 const server = http.createServer((req, res) => {
   if (req.url === "/users") {
     res.status = 200;
     res.statusMessage = "OK";
     res.headers = "Content-Type: application/json";
-    res.write("{}");
+    res.write(getUsers());
     res.end();
 
     return;
@@ -20,4 +25,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(3003, () => {
   console.log("Server is running on address http://127.0.0.1:3003");
-})
+});
